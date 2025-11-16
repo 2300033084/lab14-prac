0da2d73b172c763358bdeae3e76dcf7f6e92cf3c
yashodhan <2300033084@kluniversity.in>
2025-11-16 17:54:09 +0530

    final prac

--- backend-jenkins/src/main/java/com/klef/dev/BackendJenkinsApplication.java
+++ backend-jenkins/src/main/java/com/klef/dev/BackendJenkinsApplication.java
@@ -2,17 +2,11 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendJenkinsApplication extends SpringBootServletInitializer {
+public class BackendJenkinsApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BackendJenkinsApplication.class);
-    }
-
+    
     public static void main(String[] args) {
         SpringApplication.run(BackendJenkinsApplication.class, args);
         System.out.println("Backend Jenkins Application is Running...");
--- frontend-jenkins/src/components/config.js
+++ frontend-jenkins/src/components/config.js
@@ -1,5 +1,5 @@
 const config = {
-    url: "http://localhost:2030/backend-jenkins"
+    url: "http://localhost:2025"
     //url: "http://localhost:2002"
 };
 
